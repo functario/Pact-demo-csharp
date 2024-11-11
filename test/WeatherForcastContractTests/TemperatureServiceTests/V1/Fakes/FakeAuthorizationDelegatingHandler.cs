63ad163eb6ab46c884e4e5cd07c42c8aeb5dc7d0
functario <166994080+functario@users.noreply.github.com>
2024-11-11 10:48:01 -0500

    Cleanup

--- test/WeatherForcastContractTests/TemperatureServiceTests/V1/Fakes/FakeAuthorizationDelegatingHandler.cs
+++ test/WeatherForcastContractTests/TemperatureServiceTests/V1/Fakes/FakeAuthorizationDelegatingHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http.Headers;
+using WeatherForcastContractTests.Fixtures.AuthenticationServiceFixtures;
 
 namespace WeatherForcastContractTests.TemperatureServiceTests.V1.Fakes;
 
@@ -10,14 +11,8 @@ public sealed class FakeAuthorizationDelegatingHandler : DelegatingHandler
     )
     {
         ArgumentNullException.ThrowIfNull(request, nameof(request));
-        // Retrieve the Bearer token from the HttpContext
-        //var token =
-        //    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzEyNjE3MjgsImlzcyI6ImF1dGhlbnRpY2F0aW9uc2VydmljZSIsImF1ZCI6IndlYXRoZXJmb3JjYXN0YXBpIn0.TMwRk-3BbhheWcWlKH4yPDbmEKgcqb5RAUipZYbH5BA";
 
-        //var token =
-        //"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6Im1vam9oIiwic3ViIjoibW9qb2giLCJqdGkiOiJkNzA1YTZkNyIsImF1ZCI6WyJodHRwOi8vbG9jYWxob3N0OjQzMjc1IiwiaHR0cHM6Ly9sb2NhbGhvc3Q6NDQzMDciLCJodHRwOi8vbG9jYWxob3N0OjUxODYiLCJodHRwczovL2xvY2FsaG9zdDo3MjA3Il0sIm5iZiI6MTczMTIzNzQxNSwiZXhwIjoxNzM5MTg2MjE1LCJpYXQiOjE3MzEyMzc0MTUsImlzcyI6ImRvdG5ldC11c2VyLWp3dHMifQ.WawK_9mRnt4uQQP62ozI2JELFYXkUGchUtpLB1-O19o";
-
-        var token = "toto";
+        var token = AuthenticationFixtures.ValidOldToken;
         if (!string.IsNullOrEmpty(token))
         {
             // Add the Authorization header to the request
